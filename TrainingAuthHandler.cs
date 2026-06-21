4aa0397c1467e3c318de96969e73df0cc78b4627
samrawitatnafu24 <samrawitatnafu24@gmail.com>
2026-06-21 20:20:07 +0300

    feat: implement request flow and visibility for guided lab

---
+++ TrainingAuthHandler.cs
@@ -0,0 +1,36 @@
+using System.Security.Claims;
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Options;
+
+public class TrainingAuthHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+{
+    public TrainingAuthHandler(
+        IOptionsMonitor<AuthenticationSchemeOptions> options,
+        ILoggerFactory logger,
+        UrlEncoder encoder)
+        : base(options, logger, encoder)
+    {
+    }
+
+    protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+    {
+        // If the request completely lacks our custom test header, turn them away as unauthenticated
+        if (!Request.Headers.ContainsKey("X-Training-User"))
+        {
+            return Task.FromResult(AuthenticateResult.Fail("Missing training user header."));
+        }
+
+        // Extract header value to assemble identities and attach successful claims
+        var claims = new[]
+        {
+            new Claim(ClaimTypes.Name, Request.Headers["X-Training-User"]!)
+        };
+
+        var identity = new ClaimsIdentity(claims, Scheme.Name);
+        var principal = new ClaimsPrincipal(identity);
+        var ticket = new AuthenticationTicket(principal, Scheme.Name);
+
+        return Task.FromResult(AuthenticateResult.Success(ticket));
+    }
+}
